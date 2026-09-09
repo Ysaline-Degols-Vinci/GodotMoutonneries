44230c748889d84689b3e5f6a69139402a1cf0ff
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2026-09-09 11:57:40 +0200

    add sheeps + try to make the scrolling work

--- scenes/shop.gd
+++ scenes/shop.gd
@@ -3,13 +3,17 @@ extends Control
 var shop_character_scene = preload("res://scenes/shop_character.tscn")
 
 func _ready():
+	print("AAAAAAAAAAAAAAAAAAAAAAA")
 	for character_state in GameData.characters:
 		
 		var shop_character = shop_character_scene.instantiate()
-		
+		print("Création de : ", character_state.data.character_name)
+
 		shop_character.setup(character_state)
 		
-		$CharacterGrid.add_child(shop_character)
+		$ScrollContainer/CharacterGrid.add_child(shop_character)
+		print("Taille du grid : ", $ScrollContainer/CharacterGrid.size)
+		print("Taille du scroll : ", $ScrollContainer.size)
 
 
 func _on_button_game_pressed() -> void:
